df3d78e662b9dfba6e9820c04b72fecf02390171
Eugenia Smith <ev-genius4@mail.ru>
2025-01-04 20:54:46 +0100

    Backend: interaction with database is set; cors lib for frontend part is installed and added to the app file

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import fs from "fs/promises";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("Hi there!");
