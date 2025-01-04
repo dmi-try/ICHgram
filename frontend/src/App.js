fd5c81f417b3a0b7a099bd01f75ed71bcf228a19
Eugenia Smith <ev-genius4@mail.ru>
2025-01-04 20:51:24 +0100

    Frontend: .env file is created; axios is installed; primary interaction with backend is set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const url = process.env.REACT_APP_BACKEND_URL;
+    axios
+      .get(`${url}/`)
+      .then((res) => {
+        setResponse(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data from server:", error);
+      });
+  }, []);
   return (
     <>
-      <p>Hey</p>
+      <h2>Connection test</h2>
+      <p>{response}</p>
     </>
   );
 }
