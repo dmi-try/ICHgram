12112f568a5e9bcba77ca0058ed4e9ad2d0f702a
Eugenia Smith <ev-genius4@mail.ru>
2025-01-20 11:25:52 +0100

    Sign up alterations; adding some more components

--- frontend/src/pages/signupPage/SignupPage.jsx
+++ frontend/src/pages/signupPage/SignupPage.jsx
@@ -1,6 +1,10 @@
 import styles from "./SignupPage.module.css";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import Input from "../../components/input/Input.jsx";
+import Button from "../../components/button/Button.jsx";
+import logo from "../../assets/images/ICHGRA 2.png";
 
 function SignupPage() {
   const [formData, setFormData] = useState({
@@ -9,6 +13,13 @@ function SignupPage() {
     password: "",
   });
   const [message, setMessage] = useState("");
+
+  const inputs = [
+    { name: "name", type: "text", placeholder: "Name" },
+    { name: "email", type: "text", placeholder: "Email" },
+    { name: "password", type: "password", placeholder: "Your password" },
+    { name: "fullname", type: "text", placeholder: "Full name" },
+  ];
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -30,44 +41,47 @@ function SignupPage() {
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          name="fullname"
-          placeholder="Full name"
-          value={formData.fullname}
-          onChange={handleChange}
-        />
-        <button type="submit">Register</button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
+    <section className={styles.sign_up_container}>
+      <div className={styles.sign_up_form_container}>
+        <picture className={styles.logo}>
+          <img src={logo} alt="" />
+        </picture>
+
+        <h3 className={styles.sign_up_subheading}>
+          Sign up to share photos and videos with your friends
+        </h3>
+        <form className={styles.sign_up_form} onSubmit={handleSubmit}>
+          {inputs.map((elem) => (
+            <Input
+              key={elem.name}
+              name={elem.name}
+              type={elem.type}
+              placeholder={elem.placeholder}
+              onChange={handleChange}
+            />
+          ))}
+
+          <p className={styles.sign_up_policy}>
+            People who use our service may have uploaded your contact
+            information to Instagram. <a href="#">Learn more</a>
+          </p>
+
+          <p className={styles.sign_up_policy}>
+            By signing up, you agree our <a href="#">Terms</a>,
+            <a href="#">Privacy Policy</a> and <a href="#">Cookies Policy</a>.
+          </p>
+
+          <Button type="submit" text="Register" />
+        </form>
+        {message && <p>{message}</p>}
+      </div>
+      <p className={styles.sign_up_login_block}>
+        Have an account?
+        <Link to="/login" className={styles.sign_up_to_login}>
+          Log In
+        </Link>
+      </p>
+    </section>
   );
 }
 
