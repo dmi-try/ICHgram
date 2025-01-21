72fe256da8a8bc44569fc7977fa8dd89f13c77c7
Eugenia Smith <ev-genius4@mail.ru>
2025-01-21 14:42:25 +0100

    Draft of the search page is composed (former UsersPage)

--- frontend/src/pages/searchPage/SearchPage.jsx
+++ frontend/src/pages/searchPage/SearchPage.jsx
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-function UsersPage() {
+function SearchPage() {
   const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
   useEffect(() => {
     const fetchUsers = async () => {
       const token = localStorage.getItem("token");
 
       try {
         const response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/users/`,
+          `${process.env.REACT_APP_BACKEND_URL}/users/?name=${filter}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -24,11 +25,16 @@ function UsersPage() {
     };
 
     fetchUsers();
-  }, []);
+  }, [filter]);
+
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+  };
 
   return (
     <section>
-      <h1>Users Page</h1>
+      <h1>Search Page</h1>
+      <p>User name: <input onChange={handleChange} /></p>
       <ul>
         {users.map((user) => (
           <li key={user._id}>
@@ -40,4 +46,4 @@ function UsersPage() {
   );
 }
 
-export default UsersPage;
+export default SearchPage;
