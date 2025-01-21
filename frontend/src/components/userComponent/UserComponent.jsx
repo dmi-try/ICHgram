e4df6c9cc419a5adb117f1013d300875990f45b4
Eugenia Smith <ev-genius4@mail.ru>
2025-01-22 00:10:49 +0100

    Feature: post header component (userComponent) is created but not yet styled

---
+++ frontend/src/components/userComponent/UserComponent.jsx
@@ -0,0 +1,39 @@
+import styles from "./UserComponent.module.css";
+import userPlaceholder from "../../assets/images/menu_profile_img_small.png";
+import { Link } from "react-router-dom";
+
+function UserComponent({ user, onFollow, onUnfollow }) {
+  return (
+    <div className={styles.user_info_container}>
+      <picture>
+        <img src={userPlaceholder} alt="" />
+      </picture>
+      <div className={styles.user_info_text}>
+        {user?.isMe ? (
+          <Link to={`/profile`} className={styles.username_link}>
+            {user.name}
+          </Link>
+        ) : (
+          <Link to={`/users/${user?._id}`} className={styles.username_link}>
+            {user?.name}
+          </Link>
+        )}
+      </div>
+      {!user?.isMe && (
+        <div>
+          {user?.isFollowing ? (
+            <button className={styles.following_button} onClick={onUnfollow}>
+              Following
+            </button>
+          ) : (
+            <button className={styles.follow_button} onClick={onFollow}>
+              Follow
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserComponent;
