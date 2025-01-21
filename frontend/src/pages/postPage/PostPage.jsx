aef42ca6e23df4b381ab551b1e92f744bddab449
Eugenia Smith <ev-genius4@mail.ru>
2025-01-22 00:13:15 +0100

    Feature: draft of separate post page is created but not yet styled

--- frontend/src/pages/postPage/PostPage.jsx
+++ frontend/src/pages/postPage/PostPage.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useParams, useNavigate } from "react-router-dom";
+import styles from "./PostPage.module.css";
+import postImg from "../../assets/images/post_img.jpg";
+import LikeCounter from "../../components/likeCounter/LikeCounter.jsx";
+import CommentCounter from "../../components/commentCounter/CommentCounter.jsx";
+import UserComponent from "../../components/userComponent/UserComponent.jsx";
 
 function PostPage() {
   const [post, setPost] = useState([]);
@@ -19,13 +24,13 @@ function PostPage() {
           },
         }
       );
+      console.log("Post fetched: ", response.data);
       setPost(response.data);
     } catch (error) {
       console.error("Error fetching post: ", error.response?.data || error);
     }
   };
   useEffect(() => {
-
     fetchPost();
   }, []);
   const handleNewComment = async () => {
@@ -51,14 +56,11 @@ function PostPage() {
     const token = localStorage.getItem("token");
 
     try {
-      await axios.delete(
-        `${process.env.REACT_APP_BACKEND_URL}/posts/${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/posts/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       navigate("/");
     } catch (error) {
       console.error("Error deleting post:", error.response?.data || error);
@@ -99,24 +101,88 @@ function PostPage() {
       console.error("Error removing like:", error.response?.data || error);
     }
   };
+
+  const handleFollow = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/users/${post.user?._id}/follow`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      fetchPost();
+    } catch (error) {
+      console.error("Error following user:", error.response?.data || error);
+    }
+  };
+  const handleUnfollow = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+      await axios.delete(
+        `${process.env.REACT_APP_BACKEND_URL}/users/${post.user?._id}/follow`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      fetchPost();
+    } catch (error) {
+      console.error("Error unfollowing user:", error.response?.data || error);
+    }
+  };
+
   return (
-    <section>
-      <h1>Post Page</h1>
-      <ul>
-        <li>ID: {post._id}</li>
-        <li>User: {post.user?.name}</li>
-        <li>Photo: {post.photo}</li>
-        <li>Text: {post.text}</li>
-        <li>Like count: {post.likeCount}</li>
-        <li>Is Liked: {post.isLiked ? "Yes" : "No"}</li>
-        <li>Comments: {post.comments?.map((comment) => (
-          <p key={comment._id}>{comment.user?.name}: {comment.text}</p>
-        ))}</li>
-      </ul>
-      <p><Link to={`/posts/${post._id}/edit`}>Edit</Link> <button onClick={handleDeletePost}>Delete post</button></p>
-      <p><button onClick={handleLike}>Like!</button><button onClick={handleUnlike}>Unlike!</button></p>
-      <textarea placeholder="Add comment" value={newComment} onChange={(e) => setNewComment(e.target.value)} /><br />
-      <button onClick={handleNewComment}>Add comment</button>
+    <section className={styles.post_container}>
+      <picture>
+        <img src={postImg} alt="" />
+        {/* <div>Photo: {post.photo}</div> */}
+      </picture>
+      <div className={styles.post_contents}>
+        <UserComponent
+          user={post.user}
+          onFollow={handleFollow}
+          onUnfollow={handleUnfollow}
+        />
+        {/* <div>ID: {post._id}</div> */}
+        {/* <div>User: {post.user?.name}</div> */}
+        <div>Text: {post.text}</div>
+        <LikeCounter
+          likes={post.likeCount}
+          isLiked={post.isLiked}
+          onLike={handleLike}
+          onUnlike={handleUnlike}
+        />
+        <div>
+          <Link to={`/posts/${post._id}/edit`}>Edit</Link>{" "}
+          <button onClick={handleDeletePost}>Delete post</button>
+        </div>
+        {/* <p>
+        <button onClick={handleLike}>Like!</button>
+        <button onClick={handleUnlike}>Unlike!</button>
+      </p> */}
+        <div>
+          Comments:{" "}
+          {post.comments?.map((comment) => (
+            <p key={comment._id}>
+              {comment.user?.name}: {comment.text}
+            </p>
+          ))}
+        </div>
+
+        <textarea
+          placeholder="Add comment"
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+        />
+        <button onClick={handleNewComment}>Add comment</button>
+      </div>
     </section>
   );
 }
