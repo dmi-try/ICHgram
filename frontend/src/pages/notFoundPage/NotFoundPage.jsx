54c74eac0c18b325c89fb90b4cc8fbe06a642e1e
Eugenia Smith <ev-genius4@mail.ru>
2025-01-21 15:40:37 +0100

    Feature: 404 page is composed and added to the project

--- frontend/src/pages/notFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/notFoundPage/NotFoundPage.jsx
@@ -1,7 +1,27 @@
+import styles from "./NotFoundPage.module.css";
+import banner from "../../assets/images/main_banner_image.jpg";
+
 function NotFoundPage() {
   return (
-    <section>
-      <h1>404 Page</h1>
+    <section className={styles.not_found_container}>
+      <div className={styles.content_container}>
+        <picture>
+          <img src={banner} alt="" />
+        </picture>
+        <div className={styles.not_found_text_content}>
+          <h1>
+            Oops! Page Not Found
+            <br />
+            (404 Error)
+          </h1>
+          <p className={styles.not_found_text}>
+            We're sorry, but the page you're looking for doesn't seem to exist.
+            <br /> If you typed the URL manually, please double-check the
+            spelling. <br /> If you clicked on a link, it may be outdated or
+            broken.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
