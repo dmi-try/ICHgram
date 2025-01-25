1aa4ae7def9c9ed46efb83e3cca7ec00946bf769
Eugenia Smith <ev-genius4@mail.ru>
2025-01-26 00:03:30 +0100

    Featured: searchPage is composed and styled

--- frontend/src/pages/searchPage/SearchPage.jsx
+++ frontend/src/pages/searchPage/SearchPage.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/input/Input";
+import styles from "./SearchPage.module.css";
+import UserComponent from "../../components/userComponent/UserComponent.jsx";
 
 function SearchPage() {
   const [users, setUsers] = useState([]);
@@ -20,8 +23,11 @@ function SearchPage() {
           }
         );
         setUsers(response.data);
+        console.log(response.data);
       } catch (error) {
-        if (error.response?.status === 401) { navigate("/login"); }
+        if (error.response?.status === 401) {
+          navigate("/login");
+        }
         console.error("Error fetching users: ", error.response?.data || error);
       }
     };
@@ -35,15 +41,24 @@ function SearchPage() {
 
   return (
     <section>
-      <h1>Search Page</h1>
-      <p>User name: <input onChange={handleChange} /></p>
-      <ul>
-        {users.map((user) => (
-          <li key={user._id}>
-            <Link to={`/users/${user._id}`}>{user.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className={styles.search_container}>
+        <h2>Search</h2>
+        <label className={styles.search_label}>
+          <Input
+            className={styles.search_input}
+            placeholder={"Search"}
+            onChange={handleChange}
+          />
+        </label>
+        <ul>
+          {users.map((user) => (
+            <UserComponent user={user} key={user._id} />
+            // <li key={user._id}>
+            //   <Link to={`/users/${user._id}`}>{user.name}</Link>
+            // </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
